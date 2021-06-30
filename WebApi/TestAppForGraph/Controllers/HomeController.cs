67e4a52424c03d1bd777731b268f63b35ad7ce00
Andrei Dzimchuk <andrei@dzimchuk.net>
2021-06-30 18:45:20 +0300

    updated MS Graph sample

--- WebApi/TestAppForGraph/Controllers/HomeController.cs
+++ WebApi/TestAppForGraph/Controllers/HomeController.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Graph;
+using Microsoft.Identity.Web;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using TestAppForGraph.Models;
 
@@ -17,10 +17,23 @@ namespace TestAppForGraph.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly GraphServiceClient graphServiceClient;
 
-        public HomeController(ILogger<HomeController> logger, GraphServiceClient graphServiceClient)
+        private readonly MicrosoftIdentityConsentAndConditionalAccessHandler consentHandler;
+
+        private string[] graphScopes = new[] { "user.read" };
+
+
+        public HomeController(ILogger<HomeController> logger,
+                              GraphServiceClient graphServiceClient,
+                              MicrosoftIdentityConsentAndConditionalAccessHandler consentHandler,
+                              IConfiguration configuration)
         {
             _logger = logger;
             this.graphServiceClient = graphServiceClient;
+            this.consentHandler = consentHandler;
+
+            // Capture the Scopes for Graph that were used in the original request for an Access token (AT) for MS Graph as
+            // they'd be needed again when requesting a fresh AT for Graph during claims challenge processing
+            graphScopes = configuration.GetValue<string>("DownstreamApi:Scopes")?.Split(' ');
         }
 
         public IActionResult Index()
@@ -28,24 +41,47 @@ namespace TestAppForGraph.Controllers
             return View();
         }
 
-        [Authorize]
+        [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
         public async Task<IActionResult> Privacy()
         {
-            var user = await graphServiceClient.Me.Request().GetAsync();
+            User currentUser = null;
+
             try
             {
-                using (var photoStream = await graphServiceClient.Me.Photo.Content.Request().GetAsync()) // MsalUiRequiredException: No account or login hint was passed to the AcquireTokenSilent call.
-                {
-                    byte[] photoByte = ((MemoryStream)photoStream).ToArray();
-                    ViewData["photo"] = Convert.ToBase64String(photoByte);
-                }
-                ViewData["name"] = user.DisplayName;
+                currentUser = await graphServiceClient.Me.Request().GetAsync();
             }
-            catch (Exception ex)
+            // Catch CAE exception from Graph SDK
+            catch (ServiceException svcex) when (svcex.Message.Contains("Continuous access evaluation resulted in claims challenge"))
             {
-                ViewData["photo"] = null;
+                try
+                {
+                    Console.WriteLine($"{svcex}");
+                    var claimChallenge = AuthenticationHeaderHelper.ExtractClaimChallengeFromHttpHeader(svcex.ResponseHeaders);
+                    consentHandler.ChallengeUser(graphScopes, claimChallenge);
+                    return new EmptyResult();
+                }
+                catch (Exception ex2)
+                {
+                    consentHandler.HandleException(ex2);
+                }
             }
 
+            try
+            {
+                // Get user photo
+                using (var photoStream = await graphServiceClient.Me.Photo.Content.Request().GetAsync())
+                {
+                    byte[] photoByte = ((MemoryStream)photoStream).ToArray();
+                    ViewData["Photo"] = Convert.ToBase64String(photoByte);
+                }
+            }
+            catch (Exception pex)
+            {
+                Console.WriteLine($"{pex.Message}");
+                ViewData["Photo"] = null;
+            }
+
+            ViewData["Me"] = currentUser;
             return View();
         }
 
