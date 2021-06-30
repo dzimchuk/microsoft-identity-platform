cbe57c8c607a23a192ebe52ce293969a150412c7
Andrei Dzimchuk <andrei@dzimchuk.net>
2021-06-30 15:02:41 +0300

    added Graph client sample

---
+++ WebApi/TestAppForGraph/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAppForGraph.Models;
+
+namespace TestAppForGraph.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly GraphServiceClient graphServiceClient;
+
+        public HomeController(ILogger<HomeController> logger, GraphServiceClient graphServiceClient)
+        {
+            _logger = logger;
+            this.graphServiceClient = graphServiceClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Privacy()
+        {
+            var user = await graphServiceClient.Me.Request().GetAsync();
+            try
+            {
+                using (var photoStream = await graphServiceClient.Me.Photo.Content.Request().GetAsync()) // MsalUiRequiredException: No account or login hint was passed to the AcquireTokenSilent call.
+                {
+                    byte[] photoByte = ((MemoryStream)photoStream).ToArray();
+                    ViewData["photo"] = Convert.ToBase64String(photoByte);
+                }
+                ViewData["name"] = user.DisplayName;
+            }
+            catch (Exception ex)
+            {
+                ViewData["photo"] = null;
+            }
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
